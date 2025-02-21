30f50823c5923e07be00f4f95cb13efa1448eec2
izzyvincent <159850507+izzyvincent@users.noreply.github.com>
2025-02-21 13:25:51 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// used OpenAI here as well as I forgot to do npx tailwindcss init and found issues in doing so after already finished most of the project.
 export default defineConfig({
   plugins: [
     react(),
